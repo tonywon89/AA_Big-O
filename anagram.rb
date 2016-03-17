dac099effd2b80eb88d5f9612bc57c0c359c58ab
Tony Won <tonywon89@gmail.com>
2016-03-17 14:48:20 -0700

    Finish anagram

--- anagram.rb
+++ anagram.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 def first_anagram?(first_word, target_word)
 
   all_anagrams = first_word.chars.permutation.to_a.map(&:join)
@@ -5,5 +7,40 @@ def first_anagram?(first_word, target_word)
 
 end
 
-p first_anagram?("gizmo", "sally")
-p first_anagram?("elvis", "lives") 
+def second_anagram?(first, target)
+  first_copy = first.dup.chars
+  target_copy = target.dup.chars
+
+  first.each_char.with_index do |letter1, i|
+    target.each_char.with_index do |letter2, j|
+      if letter1 == letter2
+        first_copy[i] = nil
+        target_copy[j] = nil
+      end
+    end
+  end
+  first_copy.all? { |c| c.nil? } && target_copy.all? { |c| c.nil? }
+end
+
+def third_anagram?(first, target)
+  first.chars.sort == target.chars.sort
+end
+
+def fourth_anagram?(first, target)
+  first_hash = Hash.new { |hash, key| hash[key] = 0 }
+  target_hash = Hash.new { |hash, key| hash[key] = 0 }
+
+  first.each_char do |letter|
+    first_hash[letter] += 1
+  end
+
+  target.each_char do |letter|
+    target_hash[letter] += 1
+  end
+
+  first_hash == target_hash
+end
+
+
+p fourth_anagram?("gizmo", "sally")
+p fourth_anagram?("elvis", "lives")
